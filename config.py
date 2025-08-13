b917ca4abb85468f66409bffc36005eada162835
JUNU555tn <105146928+JUNU555tn@users.noreply.github.com>
2025-08-13 17:49:58 +0600

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config(object):
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://telegra.ph/file/1efd13f55ef33d64aa2c8.jpg")
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://i.ibb.co/RGJzsbL5/x.jpg")
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
@@ -30,3 +30,4 @@ class Config(object):
     DEF_WATER_MARK_FILE = ""
     #Admin id is stored in 
     LAZY_DEVELOPER = set(int(x) for x in os.environ.get("LAZY_ADMIN", "6415368038").split())
+
